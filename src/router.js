5ba510ad87e8aa8469fca632d0185f3fc99e5375
Abram <israelvictory87@gmail.com>
2022-09-03 18:45:53 +0100

    Created route

--- src/router.js
+++ src/router.js
@@ -2,34 +2,34 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
     {
         path: "/",
-        alias: "/books",
+        alias: "/books/",
         name: "books",
         component: () => import("./components/GetBooks")
     },
     {
         path: "/authors/",
         name: "authors",
-        component: () => import("./components/GetBooks")
+        component: () => import("./components/GetAuthors")
     },
     {
         path: "/book/:id/",
         name: "book-details",
-        component: () => import("./components/")
+        component: () => import("./components/DetailUpdateBook")
     },
     {
         path: "/author/:id/",
         name: "author-details",
-        component: () => import("./components/")
+        component: () => import("./components/DetailUpdateAuthor")
     },
     {
         path: "/author/",
         name: "author",
-        component: () => import("./components/CreateBook.vue")
+        component: () => import("./components/CreateAuthor")
     },
     {
         path: "/book/",
         name: "book",
-        component: () => import("./components/CreateBook.vue")
+        component: () => import("./components/CreateBook")
     },
 ];
 
